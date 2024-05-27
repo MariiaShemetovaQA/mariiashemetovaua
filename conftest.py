b96072b4c665443ec3930e497887c3e1243220e0
Mariia Shemetova <mariia.shemetova@gmail.com>
2024-05-27 13:58:00 +0200

    added new api client, added new tests

--- conftest.py
+++ conftest.py
@@ -1,4 +1,6 @@
 import pytest
+from modules.api.clients.github import GitHub
+
 
 class User:
 
@@ -14,6 +16,7 @@ class User:
         self.name = ''
         self.second_name = ''
 
+
 @pytest.fixture
 def user():
     user = User()
@@ -22,3 +25,9 @@ def user():
     yield user
 
     user.remove()
+
+
+@pytest.fixture
+def github_api():
+    api = GitHub()
+    yield api
